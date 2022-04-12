f258dc721acecaa0044badb6046baaa38eaeb111
sopaconk22 <102839048+sopaconk22@users.noreply.github.com>
2022-04-11 20:54:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ var link =document.getElementById("dondever");
 var fuente=document.getElementById("fuente");
 
 function aleatorio(){
-  var randomproblem=Math.floor(Math.random()*64)+1;
+  var randomproblem=Math.floor(Math.random()*79)+1;
   if(randomproblem==1){fuente.textContent="2022.G9.1 Acumulativo ONMAPS Jalisco"; dondever.src="problemas/P1.jpg";}
   else if(randomproblem==2){fuente.textContent="2022.G9.2 Acumulativo ONMAPS Jalisco"; dondever.src="problemas/P2.jpg";}
   else if(randomproblem==3){fuente.textContent="2022.G9.3 Acumulativo ONMAPS Jalisco"; dondever.src="problemas/P3.jpg";}
